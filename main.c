69cc0f65509484f4ddab0cba464d19d9429d2cdd
hoang quoc bao <hoangquocbao20920@gmail.com>
2022-12-25 20:51:37 +0700

    Commit again main.c file

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(void)
 {
+    uint8_t u_sum = 0;
+    uint8_t u_a1 = 0;
+    uint8_t u_a2 = 0;
+
+    printf("Nhap a1 = ");
+    scanf("%d", &u_a1);
+    printf("Nhap a2 = ");
+    scanf("%d", &u_a2);
+
     return 0;
 }
